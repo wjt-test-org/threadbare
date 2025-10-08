5488e5efc023224c80434d5954cb50d7ec54df79
felixwalberg <122999576+felixwalberg@users.noreply.github.com>
2025-10-08 10:17:46 -0400

    Add Nora and the Impossible Homework StoryQuest (#1301)
    Nora and the Nearly Impossible Homework follows a student through their
    struggles to perform at a high academic level. They deal with
    self-doubt, shame, and frustration. But, through determination and the
    support of a thoughtful friend, they are able to persevere and attain
    what they once thought wasn't possible. Each mini-game correlates with
    an emotion: first anger, then sadness, then happiness!
    
    All art either created by @felixwalberg, or based on modifying existing
    artwork in the game.
    
    Engineering:
    
    - Anger Level by @tovedetered
    - Stealth Level by @felixwalberg
    - Happiness Level by @felixwalberg
    
    Game Design: 
    
    - Core Gameplay & Mechanics by @felixwalberg and @tovedetered
    - Progression & Motivation by @felixwalberg and @tovedetered
    - Narrative & Storytelling by @aureliamarzipan, @felixwalberg and
    @tovedetered
    
    Production and Management:
    
    - Code review by @felixwalberg and @tovedetered
    
    Co-authored-by: Aurelia <aureliakornheiser@gmail.com>
    Co-authored-by: Spenser Kramer <skramer3@uvm.edu>
    Co-authored-by: izzykriz <43703712+izzykriz@users.noreply.github.com>

---
+++ scenes/quests/story_quests/verso/1_verso_combat_anger/verso_combat_components/throwing_enemy_health.gd
@@ -0,0 +1,39 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
+@tool
+extends ThrowingEnemy
+
+signal Defeated
+
+func shoot_projectile() -> void:
+	var player: Player = get_tree().get_first_node_in_group("player")
+	if not is_instance_valid(player):
+		return
+	if not allowed_labels:
+		_is_attacking = false
+		return
+	var projectile: Projectile = PROJECTILE_SCENE.instantiate()
+	projectile.direction = projectile_marker.global_position.direction_to(player.global_position)
+	scale.x = 1 if projectile.direction.x < 0 else -1
+	projectile.label = allowed_labels.pick_random()
+	if projectile.label in color_per_label:
+		projectile.color = color_per_label[projectile.label]
+	projectile.global_position = (projectile_marker.global_position + (projectile.direction * 75.))
+	if projectile_follows_player:
+		projectile.node_to_follow = player
+	projectile.sprite_frames = projectile_sprite_frames
+	projectile.hit_sound_stream = projectile_hit_sound_stream
+	projectile.small_fx_scene = projectile_small_fx_scene
+	projectile.big_fx_scene = projectile_big_fx_scene
+	projectile.trail_fx_scene = projectile_trail_fx_scene
+	projectile.speed = projectile_speed
+	projectile.duration = projectile_duration
+	projectile.can_hit_enemy = true
+	get_tree().current_scene.add_child(projectile)
+	_set_target_position()
+	_is_attacking = false
+	
+func _on_got_hit(body: Node2D) -> void:
+	super(body)
+	Defeated.emit()
+	remove()
